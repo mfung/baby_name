0dd676d4971156bcbae01a4cb8ca345e73e6a1b1
Meng Fung <meng.fung@gmail.com>
2020-09-30 18:26:44 -0700

    Updated top 100

--- app/controllers/top100_controller.rb
+++ app/controllers/top100_controller.rb
@@ -1,4 +1,6 @@
 class Top100Controller < ApplicationController
   def show
+    @female_names = Name.where(gender: 'F', year: params[:year]).where('rank <= 100').order(rank: :asc)
+    @male_names = Name.where(gender: 'M', year: params[:year]).where('rank <= 100').order(rank: :asc)
   end
 end
